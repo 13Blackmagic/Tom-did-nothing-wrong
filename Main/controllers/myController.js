4324002b492b73263b6bbb3a8c256b0aa9ac6a92
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-06-29 19:08:22 -0600

    adding in controllers

--- Main/controllers/myController.js
+++ Main/controllers/myController.js
@@ -1,1 +1,117 @@
+const {User, My} = require('../models');
+
+const userSeeds = require('./userSeeds.json');
+const mySeeds = require('./mySeeds.json');
+
+const seedDatabase = async () => {
+    await User.deleteMany();
+    
+    await User.create(userSeeds);
+    
+    await My.deleteMany();
+    
+    await My.create(mySeeds);
+    
+    process.exit(0);
+    }
+
+    module.exports = {
+        async getUsers(req, res) {
+            try {
+                const users = await User.find();
+                return res.json(users);
+            }
+            catch (err) {
+                console.log(err);
+                return res.status(400).json(err);
+            }
+        }
+    },
+
+    async getMy(req, res) {
+        try {
+            const my = await My.find();
+            return res.json(my);
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(400).json(err);
+        }
+    }
+
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            return res.json(user);
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(400).json(err);
+        }
+    }
+
+    async createMy(req, res) {
+        try {
+            const my = await My.create(req.body);
+            return res.json(my);
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(400).json(err);
+        }
+    }
+
+    async deleteUsers(req, res) {
+        try {
+            const user = await User.deleteMany();
+            return res.json(user);
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(400).json(err);
+        }
+    }
+
+    async deleteMy(req, res) {
+        try {
+            const my = await My.deleteMany();
+            return res.json(my);
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(400).json(err);
+        }
+    }
+
+    async updateUser(req, res) {
+        try {
+            const user = await User.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                { new: true }
+            );
+            return res.json(user);
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(400).json(err);
+        }
+    }
+
+    async updateMy(req, res) {
+        try {
+            const my = await My.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                { new: true }
+            );
+            return res.json(my);
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(400).json(err);
+        }
+    }
+
+
 
