3f818c79a015593c54b21b8f144eafa944a8a066
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-07-14 22:42:27 -0600

    fixing grammer

--- Main/routes/api/userRoutes.js
+++ Main/routes/api/userRoutes.js
@@ -3,10 +3,15 @@ const{
     getUsers,
     getSingleUser,
     createUser,
+    deleteUser,
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser).post(createUser);
+
+router.route('/:userId').delete(deleteUser).put(createUser);
+
+
 
 module.exports = router;
